803c03f18a79e7ec9bcd1dc53c7ef60b9a039343
mazzaKaio <mazza.kaio12@gmail.com>
2025-07-08 22:07:34 -0300

    Realizando exemplos sobre PHP

---
+++ primeiro_programa.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Programa - PHP</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Olá mundo, PHP-7 que vos fala!</h1>";
+        phpinfo();
+    ?>
+</body>
+</html>
---
+++ saida.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saída - PHP</title>
+</head>
+<body>
+    <?php
+        print "Teste<br>";
+        print "Olá, mundo!<br>";
+        print "Escape 'chars' são os MESMOS como em C\n<br>";
+        print "Você pode ter quebras de linhas em uma string.<br>";
+        print 'Uma string pode usar "aspas-duplas". Isso é muito legal!<br>';
+        print 'Ainda pode-se usar aspas simples dessas forma It\'s cool!<br><br>';
+
+        echo "texto<br>";
+        echo "Olá mundo!<br>";
+        echo "Isso abrange
+        várias linhas. As novas linhas serão
+        saída também.<br>";
+        echo "Isso abrange\nmultiplas linhas. A nova linha será \na saida também.<br>";
+        echo "Caracteres Espacing são feitos \"Como esse\".<br>";
+
+        // Cristo ou /bas
+    ?>
+</body>
+</html>
