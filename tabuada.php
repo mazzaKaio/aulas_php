4e75bd732dcd63f4b0b31d52ecfea5aad82a2833
mazzaKaio <mazza.kaio12@gmail.com>
2025-07-15 21:17:39 -0300

    Exercicio - Fazer tabuada com WHILE

---
+++ tabuada.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabuaba com WHILE - PHP</title>
+</head>
+<body>
+    <?php
+        $i = 1;
+        $numero = 1;
+
+        while ($numero <= 10){
+
+            while ($i <= 10){
+                echo "$numero X $i = " . $numero * $i . "<br>";
+                $i++;
+            }
+
+            echo "<br>";
+            $numero++;
+            $i = 1;
+
+        }
+    ?>
+</body>
+</html>
