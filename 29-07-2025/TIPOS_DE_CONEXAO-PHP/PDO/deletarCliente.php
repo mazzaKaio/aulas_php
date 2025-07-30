d3d5459ba4de3988c5011b6bf94e4233ae3dc4c1
mazzaKaio <mazza.kaio12@gmail.com>
2025-07-30 19:26:20 -0300

    Método para processar atualização

--- 29-07-2025/TIPOS_DE_CONEXAO-PHP/PDO/deletarCliente.php
+++ 29-07-2025/TIPOS_DE_CONEXAO-PHP/PDO/deletarCliente.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Excluir Cliente</title>
 </head>
 <body>
-    
+    <h2>Excluir Cliente</h2>
 </body>
 </html>
