c17be6f47e8477f64674aabae9ab7d317e43090e
Kaio Mazza <mazza.kaio12@gmail.com>
2025-08-07 22:13:04 -0300

    Rapido

--- PROJETO_ImagemFuncionario/consulta_funcionario.php
+++ PROJETO_ImagemFuncionario/consulta_funcionario.php
@@ -1,3 +1,67 @@
 <?php
-    
+    // CONFIGURAÇÃO DO BANCO DE DADOS
+    $host = "localhost";
+    $database = "armazena_imagens";
+    $user = "root";
+    $pass = "";
+
+    try {
+        // CONEXAO COM O BANCO USNADO 'PDO'
+        $pdo = new PDO("mysql:host=$host; dbname=$database", $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // RECUPERA TODOS OS FUNCIONARIOS DO BANCO DE DADOS
+        $query = "SELECT id, nome FROM funcionarios";
+
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+        $funcionarios = $stmt->fetchAll(PDO::FETCH_ASSOC); // BUSCA TODOS OS RESULTADOS COMO UMA MATRIZ ASSOCIATIVA
+
+        // VERIFICA SE FOI SOLICITADO A EXCLUSÃO DE UM FUNCIONARIO
+        if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['excluir_id'])) {
+            $excluir_id = $_POST['excluir_id'];
+
+            $query_exclusao = "DELETE FROM funcionarios WHERE id=:id";
+
+            $stmt_exclusao = $pdo->prepare($query_exclusao);
+            $stmt_exclusao->bindParam(':id', $excluir_id, PDO::PARAM_INT);
+            $stmt_exclusao->execute();
+
+            // REDIRECIONA PARA EVITAR REENVIO DO FORMUALRIO
+            header('Location: '.$_SERVER['PHP_SELF']);
+            exit();
+        }
+    } catch (PDOExcpetion $e) {
+        echo "Erro: ".$e->getMessage();
+    }
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consulta de Funcionarios</title>
+</head>
+<body>
+    <h1>Consulta de Funcionários</h1>
+
+    <ul>
+        <?php foreach($funcionarios as $funcionario): ?>
+            <li>
+                <!-- O CÓDIGO ABAIXO CRIA LINK PARA VISUALIZAR DETALHES DO FUNCIONÁRIO -->
+                <a href="visualizar_funcionario.php?id=<?= $funcionario['id'] ?>">
+                    <?= htmlspecialchars($funcionario['nome' ])?>
+                </a>
+                
+                <!-- FORMULARIO PARA EXCLUIR FUNCIONARIO -->
+                 <form method="POST" style="display: inline;">
+                    <input type="hidden" name="excluir_id" value="<?= $funcionario['id'] ?>">
+
+                    <button type="submit">Excluir</button>
+                 </form>
+            </li>
+            <?php endforeach; ?>
+    </ul>
+</body>
+</html>
---
+++ PROJETO_ImagemFuncionario/visualizar_funcionario.php
@@ -0,0 +1,60 @@
+<?php
+// INFORMAÇÕES DO BANCO DE DADOS
+    $host = "localhost";
+    $database = "armazena_imagens";
+    $user = "root";
+    $pass = "";
+
+    try {
+        // CONEXAO COM O BANCO USNADO 'PDO'
+        $pdo = new PDO("mysql:host=$host; dbname=$database", $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+
+            $query = "SELECT nome, telefone, tipo_foto, foto FROM funcionarios WHERE id = :id";
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                $funcionario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['excluir_id'])) {
+                    $excluir_id = $_POST['excluir_id'];
+
+                    $query_exclusao = "DELETE FROM funcionario WHERE id = :id";
+
+                    $stmt_exclusao = $pdo->prepare($query_exclusao);
+                    $stmt_exclusao->bindParam(":id", $excluir_id, PDO::PARAM_INT);
+                    $stmt_exclusao->execute();
+
+                    header('Location: consulta_funcionario.php');
+                    exit();
+                }
+
+                ?>
+                <!DOCTYPE html>
+                <html lang="pt-br">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Visualizar Funcionário</title>
+                </head>
+                <body>
+                    <h1>Visualizar Funcionário</h1>
+
+                    <p>Nome: <?= htmlspecialchars($funcionario['nome']) ?></p>
+                    <p>Telefone: <?= htmlspecialchars($funcionario['telefone']) ?></p>
+                    <p>Tipo foto: <?= htmlspecialchars($funcionario['tipo_foto']) ?></p>
+                    <p>Foto: <?= htmlspecialchars($funcionario['foto']) ?></p>
+                </body>
+                </html>
+                <?php
+            }
+        }
+    } catch (PDOExcpetion $e) {
+        echo "Só para testar: ">$e->getMessage();
+    }
+?>
